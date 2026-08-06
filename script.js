05aee970510e3e94b87bdca9c4e94da6ad6be83c
drish <drishtismail+git@gmail.com>
2026-08-07 01:00:26 +0530

    worked on UI

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log("hello i am drish and i am making a weather app using javascript and html and css");
+const button = document.getElementById("searchbtn");
+button.addEventListener("click", function() {
+    console.log("button clicked");
+});
