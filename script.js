80966eabec596781057f1b34af6a4826a315c939
drish <drishtismail+git@gmail.com>
2026-08-17 00:39:15 +0530

    fetched the api!!

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-console.log("hello i am drish and i am making a weather app using javascript and html and css");
 const button = document.getElementById("searchbtn");
 button.addEventListener("click", function() {
-    console.log("button clicked");
+    document.getElementById("today").style.fontSize = "90px";
 });
+function something() {
+    document.getElementById("searchbtn").style.fontSize = "100px";
+}
+button.addEventListener("click", function() {
+    let city = document.getElementById("searchcity").value;
+    const url2="http://api.openweathermap.org/geo/1.0/direct?q="+city+"&appid=a7c560aa1b68b9fc9316086b56c59736";    
+    fetch(url2)
+        .then(response=> response.json())
+        .then(data=>{console.log(data);
+            let lat=data[0].lat;
+            let long=data[0].lon;
+            const url="https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&appid=a7c560aa1b68b9fc9316086b56c59736";
+            fetch(url)
+                .then(response=> response.json())
+                .then(data=> console.log(data))
+                .catch(error=> console.error("error error!!!!", error));
+});
+});
+        
+   
+
