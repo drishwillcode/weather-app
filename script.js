4be5de8918e5056f36cffccfdf3a09c701aed919
drish <drishtismail+git@gmail.com>
2026-08-21 00:01:04 +0530

    works

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 const button = document.getElementById("searchbtn");
 button.addEventListener("click", function() {
-    document.getElementById("today").style.fontSize = "90px";
-});
-function something() {
-    document.getElementById("searchbtn").style.fontSize = "100px";
-}
-button.addEventListener("click", function() {
     let city = document.getElementById("searchcity").value;
     const url2="http://api.openweathermap.org/geo/1.0/direct?q="+city+"&appid=a7c560aa1b68b9fc9316086b56c59736";    
     fetch(url2)
@@ -13,10 +7,14 @@ button.addEventListener("click", function() {
         .then(data=>{console.log(data);
             let lat=data[0].lat;
             let long=data[0].lon;
-            const url="https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&appid=a7c560aa1b68b9fc9316086b56c59736";
+            const url="https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&units=metric&appid=a7c560aa1b68b9fc9316086b56c59736";
             fetch(url)
                 .then(response=> response.json())
-                .then(data=> console.log(data))
+                .then(data=> {
+                    console.log(data);
+                    document.getElementById("city").textContent =data.name;
+                    document.getElementById("temperature").textContent =data.main.temp;
+                })
                 .catch(error=> console.error("error error!!!!", error));
 });
 });
