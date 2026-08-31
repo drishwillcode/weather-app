835cc161b4799eaf9e39e6bf62563fc1757975e4
drish <drishtismail+git@gmail.com>
2026-08-31 23:47:10 +0530

    some corrections

--- app.py
+++ app.py
@@ -1,29 +1,17 @@
 from flask import Flask,render_template,jsonify,request
-import requests
-
+from weather import main 
 
 
 app = Flask(__name__)
 
-api_key='a7c560aa1b68b9fc9316086b56c59736'
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-@app.route("/weather")
+@app.route('/weather')
 def weather():
     city=request.args.get("city")
-    url = "https://api.openweathermap.org/data/2.5/weather"
-
-    params={
-        'q':city,
-        'appid':api_key
-    }
-    response=requests.get(url,params=params)
-
-    return jsonify(response.json())
-
-
+    data=main(city)
+    return jsonify(data.__dict__)
 if __name__ == "__main__":
     app.run(debug=True)
